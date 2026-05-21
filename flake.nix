26d04c513a2ccc826d79cde55622fe2f367a1673
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-05-21 17:08:50 +0530

    updated flake for nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "dev env for vivy";
+  description = "dev env and build package for vivy (C++ / SDL2)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,27 +15,46 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "vivy";
+          version = "0.1.0";
+          src = ./.;
+
+          # (compilers, build engines)
+          nativeBuildInputs = with pkgs; [
+            gcc
+            cmake
+            pkg-config
+          ];
+
+          # runtime shared libraries needed by your C++ binary
+          buildInputs = with pkgs; [
+            SDL2
+            SDL2_image
+            libwebp
+            libpng
+            libjpeg
+            libtiff
+            zlib
+          ];
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             gcc
             gdb
             cmake
             pkg-config
-
             SDL2
             SDL2_image
-
             libwebp
             libpng
             libjpeg
             libtiff
             zlib
-
             clang-tools
           ];
         };
