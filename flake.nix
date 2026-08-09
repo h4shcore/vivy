f3a91da65ed32f480d7fc5838f6b22679afb7a07
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-08-10 00:39:22 +0530

    fixed flake.nix to make it run through flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,46 @@
 {
-  description = "Development environment for vivy image viewer";
+  description = "vivy - Minimalist C++20 SDL2 Image Viewer";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cmake
-            pkg-config
-            gcc
-            SDL2
-            SDL2_image
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "vivy";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [
+              pkgs.SDL2
+              pkgs.SDL2_image
+            ];
+
+            buildPhase = "make";
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/vivy $out/bin/vivy
+            '';
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+          };
+        });
+    };
 }
